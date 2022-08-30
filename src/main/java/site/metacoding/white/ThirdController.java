49d68bc01fcdfe1c2455fe21afdb25c9a18f6205
basakk123 <basakk@naver.com>
2022-08-30 16:51:59 +0900

    스프링부트 컨트롤러 데이터 응답하기

--- src/main/java/site/metacoding/white/ThirdController.java
+++ src/main/java/site/metacoding/white/ThirdController.java
@@ -1,6 +1,5 @@
 package site.metacoding.white;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
